bc41830b9876a37014ad1ce9830171a46f2cec9c
KyleCI <69059696+KyleCI@users.noreply.github.com>
2021-01-14 17:16:14 +0100

    Update

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -1,4 +1,4 @@
-"""blogapp URL Configuration
+"""blogproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
